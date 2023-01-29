40b70a90bf35f4c7c9bb1104a914b8fe3e038b92
sedaaydin360drc <seda.aydin@360drctech.com>
2023-01-29 22:08:57 +0300

    first commit

---
+++ src/PackageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace sedaaydin360drc\greetings;
+
+use App\Http\Controllers\Controller;
+
+class PackageController extends Controller
+{
+    public function index()
+    {
+        return view('sedaaydin360drc::index');
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,3 @@
+<?php
+
+\Illuminate\Support\Facades\Route::get('/greetings', [\sedaaydin360drc\greetings\PackageController::class, 'index']);
