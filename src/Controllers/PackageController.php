df77dc707ab892b6e5b62869fdf3812ad6a1ed16
sedaaydin360drc <seda.aydin@360drctech.com>
2023-01-31 10:07:12 +0300

    edited

---
+++ src/Controllers/PackageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace sedaaydin360drc\greetings\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PackageController extends Controller
+{
+    public function index()
+    {
+        return view('sedaaydin360drc::index');
+    }
+}
---
+++ src/Providers/ApexServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace sedaaydin360drc\greetings\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use sedaaydin360drc\greetings\Controllers\PackageController;
+
+class ApexServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make(PackageController::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '././routes.php');
+        $this->loadViewsFrom(__DIR__ . '././views', 'sedaaydin360drc');
+    }
+}
